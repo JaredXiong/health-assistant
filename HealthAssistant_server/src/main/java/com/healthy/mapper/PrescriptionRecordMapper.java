8d7e422ea1d75710abac02a8b510c77c27a098f2
“JaredXiong” <xijtao@hotmail.com>
2026-03-31 20:30:26 +0800

    修复一些警告

--- HealthAssistant_server/src/main/java/com/healthy/mapper/PrescriptionRecordMapper.java
+++ HealthAssistant_server/src/main/java/com/healthy/mapper/PrescriptionRecordMapper.java
@@ -4,8 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.healthy.entity.PrescriptionRecord;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Map;
 
 public interface PrescriptionRecordMapper extends BaseMapper<PrescriptionRecord> {
-    Map<String, Object> getMonthlyCount(@Param("userId") Long userId);
+    List<Map<String, Object>> getMonthlyCount(@Param("userId") Long userId);
 }
